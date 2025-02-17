d9a6bf77afcedb5eb34a244c2733826dcf00ea47
roshanlanghi <roshanlanghi28@gmail.com>
2025-02-17 22:23:26 +0530

    inorder and postorder code added

---
+++ inorder.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+class BT{
+ public:
+ int data;
+ BT*left;
+ BT*right;
+ //constructor
+ BT(int x)
+ {
+   data=x;
+   left=NULL;
+   right=NULL;
+
+ }
+
+
+
+};
+
+BT* create(){
+int val;
+cout<<"accept the value from the user:"<<endl;
+cin>>val;
+
+
+
+if(val=-1)
+{
+    return NULL;
+}
+
+BT* newnode=new BT(val);
+       cout << "enter thhe right child of " << val << endl;
+
+    newnode->right = create();
+    cout << "enter thhe left child of " << val << endl;
+    newnode->left = create();
+    return newnode;  
+}   
+   
+void display(BT *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+   
+    display(root->left);
+    cout << root->data << " ";
+    display(root->right);
+   
+}
+
+
+int main()
+{
+    BT *root = create();
+    display(root);
+
+
+}
